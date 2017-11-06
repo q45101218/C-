2aeca5643908f4947a399680b6777ef4f72973f7
Yangkun <45101218@qq.com>
2017-11-06 00:44:24 -0800

    fix

--- String/String.hpp
+++ String/String.hpp
@@ -12,47 +12,90 @@ using namespace std;
 class String
 {
 public:
-    String(const char* ptr="")
-    :_sptr(new char [strlen(ptr)+1])
+    String(const char* str="")
+    :_size(strlen(str))
+    ,_capacity(size)
+    ,_str(new char [capacity+1])
     {
-        strcpy(_sptr,ptr);
+        strcpy(_str,str);
     }
 
     String(const String& s)
-    :_sptr(new char [strlen(s._sptr)+1])
+    :_size(strlen(s._size))
+    ,_capacity(_size)
+    ,_str(new char [_capacity+1])
     {
-        strcpy(_sptr,s._sptr);
+        strcpy(_str,s._str);
     }
+
+    //string(const String& s)
+    //:_size(0)
+    //,_capacity(0)
+    //,_str(NULL)
+    //{
+    //      String tmp(s._str);
+    //      Swap(tmp);
+    //}
 
     String& operator=(String s)
     {
-        swap(_sptr,s._sptr);
+        swap(_capacity,s._capacity);
+        swap(_size,s._size);
+        swap(_str,s._str);
+        //Swap(s);
         return *this;
     }
 
     ~String()
     {
-        if(NULL!=_sptr)
+        if(NULL!=_str)
         {
-            delete[] _sptr;
-            _sptr=NULL;
+            delete[] _str;
+            _str=NULL;
         }
+    }
+
+
+    char* Getstr()
+    {
+        return _str;
+    }
+
+    void Swap(String& s)
+    {
+        swap(_size,s._size);
+        swap(_capacity,s._capacity);
+        swap(_str,s._str);
     }
 
     size_t Size()const
     {
-       strlen(_sptr);
+       return _size;
     }
 
     size_t Length()const
     {
-        strlen(_sptr);
+        return _size;
+    }
+
+    void Expand(size_t n)
+    {
+        if(n>_capacity)
+        {
+            char* tmp=new char[n+1];
+            strcpy(tmp,_str);
+            delete[] _str;
+            _str=tmp;
+            _capacity=n;
+        }
     }
 
     void Print()const
     {
-        cout<<_sptr<<endl;
+        cout<<_str<<endl;
     }
 private:
-    char* _sptr;
+    char* _str;
+    size_t _size;
+    size_t _capacity;
 };
